f2e1ddb218e82127de4987bfff9e017f8d61b477
erinspyg <159609701+erinspyg@users.noreply.github.com>
2025-06-15 01:00:09 +0700

    Fix PORT for deployment

--- backend/server.js
+++ backend/server.js
@@ -225,6 +225,8 @@ app.get(/^\/(?!api|images).*/, (req, res) => {
 });
 
 // ------------------ START SERVER ------------------
-app.listen(3000, () => {
-  console.log('🚀 Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
+
